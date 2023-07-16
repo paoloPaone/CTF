5522d1988594e9a3ca9c8805413ac46dc25e5eee
paoloPaone <pao.paone@stud.uniroma3.it>
2023-07-16 17:40:06 +0200

    forse cel ho fatta

--- src/dev_manage.py
+++ src/dev_manage.py
@@ -3,18 +3,21 @@
 import os
 import sys
 
-
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_gameserver.web.dev_settings')
+
+    # Importa il modulo socket per configurare l'indirizzo IP e la porta
+    import socket
     
     # Modifica l'indirizzo IP e la porta a cui il server si collega
-    # da 127.0.0.1:8000 a 0.0.0.0:8000
+    addr = '0.0.0.0'  # Modifica l'indirizzo IP
+    port = 8000  # Modifica la porta
+
+    # Imposta l'indirizzo IP e la porta come variabili d'ambiente per Django
+    os.environ["RUNSERVER_ADDR"] = addr
+    os.environ["RUNSERVER_PORT"] = str(port)
+    
+    # Esegui il server Django
     from django.core.management import execute_from_command_line
-    from django.core.servers.basehttp import get_internal_wsgi_application
-    from django.core.management.commands.runserver import Command as runserver
-    
-    runserver.default_addr = '0.0.0.0'
-    runserver.default_port = '8000'
-    runserver.default_ipv6 = False
-    
     execute_from_command_line(sys.argv)
+
