c84a7c5d60b073a7327fbc3578a34a2ee6772fda
paoloPaone <pao.paone@stud.uniroma3.it>
2023-07-16 17:55:48 +0200

    Ip

--- src/dev_manage.py
+++ src/dev_manage.py
@@ -3,10 +3,17 @@
 import os
 import sys
 
+
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_gameserver.web.dev_settings')
+    
+    # Modifica l'indirizzo IP e la porta a cui il server si collega
+    # da 127.0.0.1:8000 a 0.0.0.0:8000
+    from django.core.management import execute_from_command_line
+    from django.core.servers.basehttp import get_internal_wsgi_application
+    from django.core.management.commands.runserver import Command as runserver
+    
 
-    # Importa il modulo socket per configurare l'indirizzo IP e la porta
     import socket
     
     # Modifica l'indirizzo IP e la porta a cui il server si collega
@@ -17,7 +24,8 @@ if __name__ == '__main__':
     os.environ["RUNSERVER_ADDR"] = addr
     os.environ["RUNSERVER_PORT"] = str(port)
     
-    # Esegui il server Django
-    from django.core.management import execute_from_command_line
+    runserver.default_addr = '0.0.0.0'
+    runserver.default_port = '8000'
+    runserver.default_ipv6 = False
+    
     execute_from_command_line(sys.argv)
-
