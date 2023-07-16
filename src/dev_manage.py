10d25e6dff8f8a5c88bcb913b3492a9f320b9591
paoloPaone <pao.paone@stud.uniroma3.it>
2023-07-16 15:48:54 +0200

    bindato su 172.17.0.1

--- src/dev_manage.py
+++ src/dev_manage.py
@@ -8,5 +8,5 @@ from django.core.management import execute_from_command_line
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_gameserver.web.dev_settings')
-    execute_from_command_line(['dev_manage.py', 'runserver', '0.0.0.0:8000'])
+    execute_from_command_line(['dev_manage.py', 'runserver', '172.17.0.1:8000'])
     
