4244a8e5618f7007434192aaf8bb755f67935af7
paoloPaone <pao.paone@stud.uniroma3.it>
2023-09-13 11:38:15 +0200

    aggiornate info orari

--- fixtures/orari.py
+++ fixtures/orari.py
@@ -1,12 +1,16 @@
 import json
 import datetime
+import pytz  # Importa il modulo pytz per gestire il fuso orario
 
 # Leggi il file JSON
 with open('master.json', 'r') as file:
     data = json.load(file)
 
-# Calcola gli orari desiderati
-current_time = datetime.datetime.now()
+# Ottieni il fuso orario "Europe/Rome"
+rome_timezone = pytz.timezone("Europe/Rome")
+
+# Calcola gli orari desiderati in base al fuso orario
+current_time = datetime.datetime.now(rome_timezone)
 service_public_time = current_time + datetime.timedelta(minutes=2)
 start_time = current_time + datetime.timedelta(minutes=5)
 end_time = start_time + datetime.timedelta(hours=1)
@@ -16,7 +20,7 @@ service_public_time = service_public_time.replace(second=0)
 start_time = start_time.replace(second=0)
 end_time = end_time.replace(second=0)
 
-# Sostituisci le variabili JSON con i valori calcolati
+# Sostituisci le variabili JSON con i valori calcolati in formato ISO 8601
 for item in data:
     if item.get("model") == "scoring.gamecontrol":
         item["fields"]["services_public"] = service_public_time.isoformat()
