ae4b8c42be0edb3ee35d3734a11d7bbf5ba07772
paoloPaone <pao.paone@stud.uniroma3.it>
2023-09-13 11:28:40 +0200

    aggiunte fixtures

--- fixtures/orari.py
+++ fixtures/orari.py
@@ -11,11 +11,18 @@ service_public_time = current_time + datetime.timedelta(minutes=2)
 start_time = current_time + datetime.timedelta(minutes=5)
 end_time = start_time + datetime.timedelta(hours=1)
 
+# Rimuovi i secondi e i microsecondi
+service_public_time = service_public_time.replace(second=0)
+start_time = start_time.replace(second=0)
+end_time = end_time.replace(second=0)
+
 # Sostituisci le variabili JSON con i valori calcolati
-data["fields"]["services_public"] = service_public_time.isoformat()
-data["fields"]["start"] = start_time.isoformat()
-data["fields"]["end"] = end_time.isoformat()
+for item in data:
+    if item.get("model") == "scoring.gamecontrol":
+        item["fields"]["services_public"] = service_public_time.isoformat()
+        item["fields"]["start"] = start_time.isoformat()
+        item["fields"]["end"] = end_time.isoformat()
 
 # Scrivi il file JSON aggiornato
-with open('config.json', 'w') as file:
+with open('master.json', 'w') as file:
     json.dump(data, file, indent=4)
